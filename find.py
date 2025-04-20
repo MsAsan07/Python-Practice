b20bb876f7afce0bd9f2978cb9287373c1474824
MsAsan07 <isagris07@gmail.com>
2025-04-20 12:49:26 +0900

    find.py

--- find.py
+++ find.py
@@ -2,4 +2,7 @@ sentence = "Let's learn more every day"
 sentence.find('r')
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 alphabet.find('z')
+alphabet.find(text[0])
+index = alphabet.find(text[0])
+print(index)
 
